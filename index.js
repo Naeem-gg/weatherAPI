67cf9be50e9ac1b8a39f4579aa10b3dc9efa797f
Naeemgg <naeemgg@duck.com>
2022-10-02 21:34:37 +0530

    whats going wrong

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const https = require("https");
+const { urlencoded } = require("express");
 const express = require("express");
-const { response } = require("express");
 const app = express();
-app.use(express.urlencoded({extended:true}))
-app.get("/", (req, res) => {
-  
-     
-    res.sendFile(`${__dirname}/enterCity.html`)
-    });
+const https = require("https")
+app.use(urlencoded({extended:true}));
 
-  
-    app.post("/",function (req,res) {
+app.get("/",function (req,res) {
 
-        let url =
-    "https://api.openweathermap.org/data/2.5/weather?q=malegaon&appid=79911f7d3cfe66b869e6b2589f7eb307&units=metric";
-  https.get(url, (response) => {
-    // console.log(response);
-    response.on("data", (data) => {
-      // console.log(data)
-      let weatherData = JSON.parse(data);
-      let mainWeather = weatherData.weather[0].main;
-      let w_desc = weatherData.weather[0].description;
-      let temp = weatherData.main.temp;
+    res.sendFile(__dirname+"/enterCity.html");
+  });
+
+app.post("/",function(req,res){
+  const city = req.body.city;
+  const apiKey = `79911f7d3cfe66b869e6b2589f7eb307`; 
+  const units = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  https.get(url,function(response){
+    
+    response.on("data",function(data){
       
-      let cityName = req.body.city;
-      // console.log(cityName);
-      res.send(`<h1>Weather Details:</h1><br><strong>By Naeem</strong><p></p>city name = ${cityName} <br> weather = ${mainWeather}<br> description = ${w_desc}<br> Temperature = ${temp}`);
+      const weatherData = JSON.parse(data);
+      const mainWeather = weatherData.weather[0].main;
+      
+      const desc = weatherData.weather[0].description;
+      const temp = weatherData.main.temp;
+      const windSpeed = weatherData.wind.speed;
+      const windDegree = weatherData.wind.deg;
+      const country = weatherData.sys.country;
+      const name = weatherData.name;
+      const long = weatherData.coord.lon;
+      const lat = weatherData.coord.lat;
+      
+
+      res.write(`<h1>${city}</h1><p>${mainWeather}.....${desc}......${temp}.....${windSpeed}</p>`)
+      
     });
+  });
 });
 
-});
-app.listen(3000, () => {
-  console.log("express server is running great! with nodemon");
+app.listen(3000,()=>{
+  console.log("server is running at port 3000");
 });
