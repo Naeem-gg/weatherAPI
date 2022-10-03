aa660d74bae5e72a91db8f11aeb314ab468e3804
Naeemgg <naeemgg@duck.com>
2022-10-03 18:13:07 +0530

    Hurray

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
-const https = require("https")
-app.use(urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}));
+const https = require("https");
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/enterCity.html");  
+});
 
-app.get("/",function (req,res) {
-
-    res.sendFile(__dirname+"/enterCity.html");
-  });
-
-app.post("/",function(req,res){
+app.post("/",function (req,res) {  
   const city = req.body.city;
-  const apiKey = "79911f7d3cfe66b869e6b2589f7eb307"; 
-  const units = "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid="+ apiKey +"&units="+ units;
-  console.log(url);
-  https.get(url,function(response){
-    
+  const apiKey ="79911f7d3cfe66b869e6b2589f7eb307";
+  const unit = "metric";
+  let url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units="+unit;
+  
+  https.get(url,function (response) {  
+    // console.log(response);
     response.on("data",function(data){
-      
-      const weatherData = JSON.parse(data);
-      const mainWeather = weatherData.weather[0].main;
-      
-      const desc = weatherData.weather[0].description;
-      const temp = weatherData.main.temp;
-      const windSpeed = weatherData.wind.speed;
-      const windDegree = weatherData.wind.deg;
-      const country = weatherData.sys.country;
-      const name = weatherData.name;
-      const long = weatherData.coord.lon;
-      const lat = weatherData.coord.lat;
-      
-
-      res.write("<h1>"+ city +"</h1><p>"+ mainWeather+ "....."+ desc +"......"+ temp+ "....."+ windSpeed +"</p>")
-      
+      let forecast = JSON.parse(data);
+      console.log(forecast);
+      let long = forecast.coord.lon;
+      let lat = forecast.coord.lat;
+      let mainWeather = forecast.weather[0].main;
+      let temp = forecast.main.temp;
+      let iconCode = forecast.weather[0].icon;
+      let icon = "https://openweathermap.org/img/wn/" +iconCode+ "@2x.png"
+      res.write("<h1>Weather details</h1>");
+      res.write("<img src="+icon+" alt='weather Icon '>");
+      res.write("<p>lon: "+long+" lat: "+lat+"</p>");
+      res.write("<p>Weather: "+mainWeather+"</p>");
+      res.write("<p>temperature: "+temp+"</p>");
+      res.write("<h3>~By Naeem~</h3>");
+      res.send();
     });
   });
 });
-
-app.listen(3000,()=>{
-  console.log("server is running at port 3000");
+let port = 3000
+app.listen(port,function(){
+  console.log("express server is running well at port: "+ port);
 });
